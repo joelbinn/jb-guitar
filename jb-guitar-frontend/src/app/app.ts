e4c92ca05756704d0de29d8c2e2785cdd919d5a9
Joel Binnquist <joel.binnquist@gmail.com>
2026-02-18 22:27:13 +0100

    some more init

--- jb-guitar-frontend/src/app/app.ts
+++ jb-guitar-frontend/src/app/app.ts
@@ -2,11 +2,9 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'jbg-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<router-outlet />`,
+  styles: ``,
 })
-export class App {
-  protected readonly title = signal('jb-guitar-frontend');
-}
+export class App {}
